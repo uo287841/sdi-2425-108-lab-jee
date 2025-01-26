c5ea6940f8730c2cbfb4b2a80f29a2c52fb4798f
uo287841 <uo287841@uniovi.es>
2025-01-26 13:50:41 +0100

    SDI-2425-108-1.2-Manejo de sesiones.

---
+++ src/main/java/com/uniovi/sdi/ServletShoppingCart.java
@@ -0,0 +1,58 @@
+package com.uniovi.sdi;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+@WebServlet(name = "ServletShoppingCart", value = "/AddToShoppingCart")
+public class ServletShoppingCart extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
+        HttpSession session = request.getSession();
+        HashMap<String, Integer> cart =
+                (HashMap<String, Integer>) session.getAttribute("cart");
+        // No hay carrito, creamos uno y lo insertamos en sesión
+        if (cart == null) {
+            cart = new HashMap<String, Integer>();
+            session.setAttribute("cart", cart);
+        }
+        String product = request.getParameter("product");
+        if (product != null) {
+            addToShoppingCart(cart, product);
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Tienda SDI: Cesta de la compra</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println(shoppingCartToHtml(cart) + "<br>");
+        out.println("<a href=\"shop.html\">Volver</a></BODY></HTML>");
+    }
+
+    private void addToShoppingCart(HashMap<String, Integer> cart, String productKey) {
+        if (cart.get(productKey) == null) {
+            cart.put(productKey, 1);
+        } else {
+            int productCount = cart.get(productKey);
+            cart.put(productKey, productCount + 1);
+        }
+    }
+
+    private String shoppingCartToHtml(HashMap<String, Integer> cart) {
+        StringBuilder shoppingCartToHtml = new StringBuilder();
+
+        for (String key : cart.keySet()) {
+            shoppingCartToHtml.append("<p>[").append(key).append("],").append(cart.get(key)).append("< unidades/p>");
+        }
+        return shoppingCartToHtml.toString();
+    }
+}
